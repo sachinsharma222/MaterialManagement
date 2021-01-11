7e8e3d79dd251a1e7572f78c0bd66edf03bdad79
mysetu <sachinsharma222@live.com>
2021-01-11 17:41:07 +0530

    bug fix

--- MaterialManagement.web/Controllers/BaseController.cs
+++ MaterialManagement.web/Controllers/BaseController.cs
@@ -1,8 +1,10 @@
 ﻿using DAL.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MaterialManagement.web.Controllers
 {
+    //[Authorize(AuthenticationSchemes = "Bearer")]
     [ApiController]
     [Route("api/[controller]")]
     public class BaseController : ControllerBase
